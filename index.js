99b14a50920764e61f7de6a2db81769c20496e7e
Fred Hohman <fred.hohman@gmail.com>
2018-01-23 01:02:39 -0500

    ignoring data for now
    update to load in specific layer

--- index.js
+++ index.js
@@ -2,8 +2,27 @@
 import * as contour from 'd3-contour';
 import * as d3 from 'd3';
 
-var layer = 1;
-var layers = [1,2,3];
+
+//  single layer
+// var layer = 6;
+
+// bible 
+var layers = [1,2,3,4,5,6,7,8,9,10,12,15];
+
+// jazz
+// var layers = [1, 2, 3, 4, 5, 10, 20, 29];
+
+console.log(d3.select("#layer-select"))
+
+d3.select("#layer-select")
+    .selectAll("option")
+    .data(layers)
+    .enter()
+    .append("option")
+    .attr("value", function(d) { return d })
+    .text(function (d) { return String(d) })
+
+d3.select("#layer-select").on('change', changeLayer)
 
 var colors = d3.scaleOrdinal(d3.schemeCategory10).domain(d3.range(1, 10));
 
@@ -18,59 +37,78 @@ var x = d3.scaleLinear()
 var y = d3.scaleLinear()
     .rangeRound([height - margin.bottom, margin.top]);
 
-d3.json("data/facebook-nips-preprocessed.json", function (error, data) {
-    if (error) throw error;
+function changeLayer() {
 
-    // console.log(data.nodes);
+    svg.selectAll("*").remove();
 
-    x.domain(d3.extent(data.nodes, function (d) { return d.location[0]; })).nice();
-    y.domain(d3.extent(data.nodes, function (d) { return d.location[1]; })).nice();
+    var layer = d3.select('#layer-select').property('value')
+    console.log(layer)
 
-    svg.insert("g", "g")
-        .attr("fill", "none")
-        .attr("stroke", function(){ return colors(layer) })
-        .attr("stroke-linejoin", "round")
-        .selectAll("path")
-        .data(contour.contourDensity()
-            .x(function (d) { return x(d.location[0]); })
-            .y(function (d) { return y(d.location[1]); })
-            .size([width, height])
-            .bandwidth(20)
-            (data.nodes.filter(function (d) { 
-                return +d.id.split('_')[1] === layer })
-            ))
-        .enter().append("path")
-        .attr("d", d3.geoPath());
+    d3.json("data/bible-l" + String(layer) + "-preprocessed.json", function (error, data) {
+    // d3.json("data/facebook-nips-preprocessed.json", function (error, data) {
+        if (error) throw error;
 
-    svg.append("g")
-        .attr("stroke", "white")
-        .attr("stroke-width", 0.5)
-        .selectAll("circle")
-        .data(data.nodes.filter(function (d) { return +d.id.split('_')[1] === layer }))
-        .enter().append("circle")
-        .attr("cx", function (d) { return x(d.location[0]); })
-        .attr("cy", function (d) { return y(d.location[1]); })
-        .attr("r", 2)
-        .attr("opacity", 0.5)
-        .attr("fill", function(d){ return '#' + d.color.split('x')[1] });
+        console.log(data.nodes.length);
 
-    svg.append("g")
-        .attr("transform", "translate(0," + (height - margin.bottom) + ")")
-        .call(d3.axisBottom(x))
-        .select(".tick:last-of-type text")
-        .select(function () { return this.parentNode.appendChild(this.cloneNode()); })
-        .attr("y", -3)
-        .attr("dy", null)
-        .attr("font-weight", "bold")
-        .text("x");
+        x.domain(d3.extent(data.nodes, function (d) { return d.location[0]; })).nice();
+        y.domain(d3.extent(data.nodes, function (d) { return d.location[1]; })).nice();
 
-    svg.append("g")
-        .attr("transform", "translate(" + margin.left + ",0)")
-        .call(d3.axisLeft(y))
-        .select(".tick:last-of-type text")
-        .select(function () { return this.parentNode.appendChild(this.cloneNode()); })
-        .attr("x", 3)
-        .attr("text-anchor", "start")
-        .attr("font-weight", "bold")
-        .text("y");
-});
+        svg.append("g")
+            .attr("stroke", "white")
+            .attr("stroke-width", 0.5)
+            .selectAll("circle")
+            .data(data.nodes.filter(function (d) {
+                // return +d.id.split('_')[1] === layer
+                return d
+            }))
+            .enter().append("circle")
+            .attr("cx", function (d) { return x(d.location[0]); })
+            .attr("cy", function (d) { return y(d.location[1]); })
+            .attr("r", 2)
+            .attr("opacity", 1.0)
+            .attr("fill", function (d) { return '#' + d.color.split('x')[1] });
+
+        svg.insert("g", "g")
+            .attr("fill", "none")
+            .attr("opacity", 0.8)
+            .attr("stroke", function () {
+                // return colors(layer)
+                return d3.select("circle").attr("fill")
+            })
+            .attr("stroke-linejoin", "round")
+            .selectAll("path")
+            .data(contour.contourDensity()
+                .x(function (d) { return x(d.location[0]); })
+                .y(function (d) { return y(d.location[1]); })
+                .size([width, height])
+                .bandwidth(60)
+                (data.nodes.filter(function (d) {
+                    // return +d.id.split('_')[1] === layer })
+                    return d
+                })
+                ))
+            .enter().append("path")
+            .attr("d", d3.geoPath());
+
+        svg.append("g")
+            .attr("transform", "translate(0," + (height - margin.bottom) + ")")
+            .call(d3.axisBottom(x))
+            .select(".tick:last-of-type text")
+            .select(function () { return this.parentNode.appendChild(this.cloneNode()); })
+            .attr("y", -3)
+            .attr("dy", null)
+            .attr("font-weight", "bold")
+            .text("x");
+
+        svg.append("g")
+            .attr("transform", "translate(" + margin.left + ",0)")
+            .call(d3.axisLeft(y))
+            .select(".tick:last-of-type text")
+            .select(function () { return this.parentNode.appendChild(this.cloneNode()); })
+            .attr("x", 3)
+            .attr("text-anchor", "start")
+            .attr("font-weight", "bold")
+            .text("y");
+    });
+
+}
