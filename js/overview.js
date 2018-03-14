651e14bd717536e9393c5ad4d22afcbdd48d0567
Fred Hohman <fred.hohman@gmail.com>
2018-03-14 14:44:49 -0400

    refactor and format overview

--- js/overview.js
+++ js/overview.js
@@ -1,21 +1,22 @@
-// overview.js
 import * as d3 from 'd3';
 var THREE = require('three');
 var TrackballControls = require('three-trackballcontrols');
-// var Stats = require('stats.js');
 // var Projector = require('three.js-projector');
 import { dataPathJSON, dataPathLayerJSON } from './index.js'
 import { GUI } from 'dat.gui/build/dat.gui.js'
-import * as $ from 'jquery';
-
-console.log('overview.js loaded')
+// import * as $ from 'jquery';
 
 export var layersUp3D = {};
 window.layersUp3D = layersUp3D;
 
 var scene = new THREE.Scene();
 scene.background = new THREE.Color(0xffffff);
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
+var camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    10000
+);
 var xCordScaleConst = 1;
 var yCordScaleConst = 1;
 var zCordHeight = 500;
@@ -23,19 +24,22 @@ var zCordHeight = 500;
 camera.position.set(0, 0, zCordHeight);
 camera.lookAt(scene.position)
 
-var overview = document.getElementById('overview-canvas-wrapper')
+var overview = document.getElementById("overview-canvas-wrapper");
 var renderer = new THREE.WebGLRenderer();
 camera.aspect = overview.clientWidth / overview.clientHeight;
 camera.updateProjectionMatrix();
-console.log(document.getElementById('overview-header').clientHeight)
-renderer.setSize(overview.clientWidth - 0, overview.clientHeight - document.getElementById('overview-header').clientHeight);
+console.log(document.getElementById("overview-header").clientHeight);
+renderer.setSize(
+    overview.clientWidth - 0,
+    overview.clientHeight - document.getElementById("overview-header").clientHeight
+);
 overview.appendChild(renderer.domElement);
 
-window.addEventListener('resize', onWindowResize, false);
-window.addEventListener('ribbonDragEnd', onWindowResize, false)
+window.addEventListener("resize", onWindowResize, false);
+window.addEventListener("ribbonDragEnd", onWindowResize, false);
 
 function onWindowResize() {
-    console.log(document.getElementById('overview-header').clientHeight)
+    // console.log(document.getElementById('overview-header').clientHeight)
     camera.aspect = overview.clientWidth / overview.clientHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(overview.clientWidth - 0, overview.clientHeight - document.getElementById('overview-header').clientHeight);
@@ -62,73 +66,60 @@ var lineMaterial = new THREE.LineBasicMaterial({
     color: 0xcccccc
 });
 
-// var lineGeometry = new THREE.Geometry();
-
 export function drawLayer3DPoints(layerNum) {
     console.log('drawing layer ' + layerNum + ' 3D points')
     layersUp3D[layerNum] = 'up'
-
+    
     d3.select("#indicator-left-" + layerNum).style("visibility", "visible");
-
+    
     d3.json(dataPathJSON, function (error, data) {
-
         if (error) {
             return console.error(error);
         }
-
-        // for (let peel = 0; peel < data.peels.length; peel++) {
-
+        
         // xCordScale = d3.scaleLinear()
         //     // .domain([0,1000])
         //     .range([0, 1000])
-
+        
         // yCordScale = d3.scaleLinear()
         //     // .domain([0,1000])
         //     .range([0, 1000])
-
+        
         zCordScale = d3.scaleLinear()
-            .domain(d3.extent(data.peels))
-            .range([-zCordHeight, zCordHeight])
+        .domain(d3.extent(data.peels))
+        .range([-zCordHeight, zCordHeight])
         window.zCordScale = zCordScale
-
+        
         var layerNumIndex = data.peels.indexOf(layerNum)
         // console.log('peel', data.peels[layerNumIndex])
-
+        
         d3.json(dataPathLayerJSON(data.peels[layerNumIndex]), function (error, layerData) {
-
+            
             for (let i = 0; i < layerData.nodes.length; i++) {
                 const node = layerData.nodes[i];
-
+                
                 var material = new THREE.MeshBasicMaterial();
                 material.side = THREE.DoubleSide;
-
+                
                 var circle = new THREE.Mesh(geometry, material);
-
+                
                 circle.scale.x = d3.select('#overview-slider-size').property('value')
                 circle.scale.y = d3.select('#overview-slider-size').property('value')
-
+                
                 // xCordScale.domain(d3.extent(layerData.nodes.map(function (item) {
                 //     return (item.x);
                 // })))
-
+                
                 // yCordScale.domain(d3.extent(layerData.nodes.map(function (item) {
                 //     return (item.y);
                 // })))
-
-                // circle.position.x = xCordScale(node.x);
+                
                 circle.position.x = node.x * d3.select('#overview-slider-spread').property('value')
-
-                // circle.position.y = yCordScale(node.y);
                 circle.position.y = -1 * node.y * d3.select('#overview-slider-spread').property('value')
-
-                // circle.position.z = zCordScale(data.peels[peel]);
                 zCordHeight = d3.select('#overview-slider-height').property('value')
                 zCordScale.range([-zCordHeight, zCordHeight])
                 circle.position.z = zCordScale(data.peels[layerNumIndex])
-
-                // circle.position.z = 1;
-                // circle.material.color.setHex(RGBtoHex(ribbonColorPeel(data.peels[layerNumIndex])))
-                // circle.material.color.setHex('0xcccccc');
+                
                 let clusterColor = d3.select("#bullet-" + data.peels[layerNumIndex]).style('fill');
                 circle.material.color.setHex(RGBtoHex(clusterColor));
                 circle.userData['id'] = node.id
@@ -137,8 +128,7 @@ export function drawLayer3DPoints(layerNum) {
                 circle.userData['y'] = -1 * node.y
                 scene.add(circle);
                 circles.push(circle)
-
-
+                
                 // var map = new THREE.TextureLoader("sprite.png");
                 // // console.log(map)
                 // var material = new THREE.SpriteMaterial({ map: map, color: 0xffffff, fog: true });
@@ -147,143 +137,143 @@ export function drawLayer3DPoints(layerNum) {
                 // sprite.position.y = node.y * 4;
                 // sprite.position.z = zCordScale(data.peels[peel]);
                 // scene.add(sprite);
-
-
+                
             }
-
-            console.log('add lines')
-            // console.log(layerData.links)
+            
+            // console.log('add lines')
             const arrayToObject = (array, keyField) =>
-                array.reduce((obj, item) => {
-                    obj[item[keyField]] = item
-                    return obj
-                }, {})
+            array.reduce((obj, item) => {
+                obj[item[keyField]] = item
+                return obj
+            }, {})
             const layerDataObject = arrayToObject(layerData.nodes, "id")
             // console.log(layerDataObject)
-
+            
             var lineGeometry = new THREE.Geometry();
-
-
-            console.log(data.peels[layerNumIndex])
+            
+            // console.log(data.peels[layerNumIndex])
             for (let j = 0; j < layerData.links.length; j++) {
                 const link = layerData.links[j];
-
-                let src = new THREE.Vector3(layerDataObject[link.source].x * d3.select('#overview-slider-spread').property('value'), -1 * layerDataObject[link.source].y * d3.select('#overview-slider-spread').property('value'), zCordScale(data.peels[layerNumIndex])),
-                    dst = new THREE.Vector3(layerDataObject[link.target].x * d3.select('#overview-slider-spread').property('value'), -1 * layerDataObject[link.target].y * d3.select('#overview-slider-spread').property('value'), zCordScale(data.peels[layerNumIndex]));
                 
-                src.source = link.source;
-                dst.target = link.target;
-                src.peel = data.peels[layerNumIndex];
-                dst.peel = data.peels[layerNumIndex];
-                src.ox = layerDataObject[link.source].x;
-                src.oy = -1 * layerDataObject[link.source].y;
-                dst.ox = layerDataObject[link.target].x;
-                dst.oy = -1 * layerDataObject[link.target].y;
-
-                lineGeometry.vertices.push(src, dst);
+                let source = new THREE.Vector3(
+                    layerDataObject[link.source].x * d3.select("#overview-slider-spread").property("value"),
+                    -1 * layerDataObject[link.source].y * d3.select("#overview-slider-spread").property("value"),
+                    zCordScale(data.peels[layerNumIndex])
+                );
+                let target = new THREE.Vector3(
+                    layerDataObject[link.target].x * d3.select("#overview-slider-spread").property("value"),
+                    -1 * layerDataObject[link.target].y * d3.select("#overview-slider-spread").property("value"),
+                    zCordScale(data.peels[layerNumIndex])
+                );
+                
+                source.source = link.source;
+                target.target = link.target;
+                source.peel = data.peels[layerNumIndex];
+                target.peel = data.peels[layerNumIndex];
+                source.ox = layerDataObject[link.source].x;
+                source.oy = -1 * layerDataObject[link.source].y;
+                target.ox = layerDataObject[link.target].x;
+                target.oy = -1 * layerDataObject[link.target].y;
+                
+                lineGeometry.vertices.push(source, target);
             }
-
+            
             var line = new THREE.LineSegments(lineGeometry, lineMaterial);
             if (d3.select('#toggle-3d-edges').property('checked')) {
                 line.visible = true;
             } else {
                 line.visible = false;
             }
-            // line.userData['peel'] = data.peels[layerNumIndex]
+            
             scene.add(line);
             lines.push(line)
-            // console.log(line)
-
-
+            
         })
-        // }
     })
 }
 
 export function drawAll3DPointsWithLayers() {
     console.log('drawing all 3D layers')
-    d3.json(dataPathJSON, function (error, data) {
 
+    d3.json(dataPathJSON, function (error, data) {
         if (error) {
             return console.error(error);
         }
-
+        
         for (let peel = 0; peel < data.peels.length; peel++) {
             if (!(data.peels[peel] in layersUp3D)) {
                 drawLayer3DPoints(data.peels[peel])
             }
         }
-    Object.keys(layersUp3D).forEach(key => {
-      d3.select("#indicator-left-" + key).style("visibility", "visible");
-    });
-
+        Object.keys(layersUp3D).forEach(key => {
+            d3.select("#indicator-left-" + key).style("visibility", "visible");
+        });
+        
     })
 }
 
-
 // overview header sliders
-d3.select('#overview-slider-size')
-    .attr('max', 10)
-    .attr('min', 0.1)
-    .attr('step', 0.1)
-    .property('value', 1)
-    .on('input', updateRadius)
-    .on('change', updateRadius)
+d3.select("#overview-slider-size")
+  .attr("max", 10)
+  .attr("min", 0.1)
+  .attr("step", 0.1)
+  .property("value", 1)
+  .on("input", updateRadius)
+  .on("change", updateRadius);
 
-var heightSlider = d3.select('#overview-slider-height')
-    .attr('max', 3 * zCordHeight)
-    .attr('min', 1)
-    .attr('step', 0.01)
-    .property('value', 1)
-    .on('input', updateZPosition)
+var heightSlider = d3.select("#overview-slider-height")
+  .attr("max", 3 * zCordHeight)
+  .attr("min", 1)
+  .attr("step", 0.01)
+  .property("value", 1)
+  .on("input", updateZPosition);
 
-d3.select('#overview-slider-spread')
-    .attr('max', 2)
-    .attr('min', 0)
-    .attr('step', 0.05)
-    .property('value', 1)
-    .on('input', updateXPosition)
+d3.select("#overview-slider-spread")
+  .attr("max", 2)
+  .attr("min", 0)
+  .attr("step", 0.05)
+  .property("value", 1)
+  .on("input", updateXPosition);
 
 d3.select('#reset-camera-button')
-    .on('click', resetOverviewCamera)
-    // .html('<i class="material-icons md-24 ">videocam</i><span style="padding-left: 5px;">Reset</span>')
-    .html('<i class="material-icons md-24 ">videocam</i> top view')
+  .on('click', resetOverviewCamera)
+  .html('<i class="material-icons md-24 ">videocam</i> top view')
+// .html('<i class="material-icons md-24 ">videocam</i><span style="padding-left: 5px;">Reset</span>')
 
 d3.select('#add-all-3d-layers')
-    .on('click', drawAll3DPointsWithLayers)
-    // .html('<i class="material-icons md-24 ">add</i><span style="padding-left: 5px;">Add All</span>')  
-    .html('<i class="material-icons md-24 ">add</i>show all')
+  .on('click', drawAll3DPointsWithLayers)
+  .html('<i class="material-icons md-24 ">add</i>show all')
+// .html('<i class="material-icons md-24 ">add</i><span style="padding-left: 5px;">Add All</span>')  
 
 d3.select('#remove-all-3d-layers')
-    .on('click', removeAll3DPoints)
-    // .html('<i class="material-icons md-24 ">add</i><span style="padding-left: 5px;">Remove All</span>')  
-    .html('<i class="material-icons md-24 ">remove</i>hide all')
+  .on('click', removeAll3DPoints)
+  .html('<i class="material-icons md-24 ">remove</i>hide all')
+// .html('<i class="material-icons md-24 ">add</i><span style="padding-left: 5px;">Remove All</span>')  
 
 d3.select('#animate-graph')
-    .on('click', animateGraph)
-    .html('<i class="material-icons md-24 ">play_arrow</i>animate')
+  .on('click', animateGraph)
+  .html('<i class="material-icons md-24 ">play_arrow</i>animate')
 
 d3.select('#toggle-3d-edges')
-    .on('click', toggle3DEdges)
-    // .html('<i class="material-icons md-24 ">play_arrow</i>show edges')
+  .on('click', toggle3DEdges)
+// .html('<i class="material-icons md-24 ">play_arrow</i>show edges')
 
 function updateRadius() {
     for (let c = 0; c < circles.length; c++) {
         circles[c].scale.x = this.value
         circles[c].scale.y = this.value
-
     }
 }
 
 function updateZPosition() {
     zCordHeight = this.value
     zCordScale.range([-zCordHeight, zCordHeight])
-
+    
     for (let c = 0; c < circles.length; c++) {
         // zCordHeight = this.value
         circles[c].position.z = zCordScale(circles[c].userData['peel'])
     }
-
+    
     for (let l = 0; l < lines.length; l++) {
         // console.log('lines', lines[l])
         for (let v = 0; v < lines[l].geometry.vertices.length; v++) {
@@ -298,7 +288,7 @@ function updateXPosition() {
         circles[c].position.x = circles[c].userData['x'] * this.value
         circles[c].position.y = circles[c].userData['y'] * this.value
     }
-
+    
     for (let l = 0; l < lines.length; l++) {
         // console.log('lines', lines[l])
         for (let v = 0; v < lines[l].geometry.vertices.length; v++) {
@@ -307,7 +297,6 @@ function updateXPosition() {
             lines[l].geometry.verticesNeedUpdate = true;
         }
     }
-
 }
 
 function resetOverviewCamera() {
@@ -324,12 +313,11 @@ function removeAll3DPoints() {
     Object.keys(layersUp3D).forEach(key => {
         d3.select("#indicator-left-" + key).style("visibility", "hidden");
     });
-
+    
     layersUp3D = {};
     circles = [];
     lines = [];
     window.layersUp3D = layersUp3D;
-
 }
 
 export function hideLayerPoints(peel) {
@@ -362,32 +350,28 @@ export function toggle3DEdges() {
 document.getElementById('toggle-3d-edges').click()
 
 function animateGraph() {
-
     console.log('animate')
     // console.log(heightSlider.property('value'))
-
+    
     var start = 1
     var end = 1000
     var duration = 2
     var increment = (end - start) / (60 * duration)
     var counter = 0
-
-    function matt() {
+    
+    function tick() {
         heightSlider.property('value', start + counter * increment)
         counter += 1
-
-        // console.log(heightSlider.property('value'))
-
+        
         if (start + increment * counter < end) {
-            window.requestAnimationFrame(matt);
+            window.requestAnimationFrame(tick);
             updateZPosition.call({ 'value': start + counter * increment })
         }
     }
-
-    window.requestAnimationFrame(matt)
-
+    
+    window.requestAnimationFrame(tick)
+    
 }
-
 
 function RGBtoHex(rgbColor) {
     var rgbColor = rgbColor.split("(")[1].split(")")[0];
@@ -399,10 +383,6 @@ function RGBtoHex(rgbColor) {
     hexColor = "0x" + hexColor.join("");
     return hexColor
 }
-
-
-
-
 
 // projector = new Projector.Projector();
 
@@ -421,22 +401,13 @@ function onDocumentMouseDown(event) {
 
 // document.addEventListener( 'mousedown', onDocumentMouseDown, false );
 
-
-
-// var stats = new Stats();
-// stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
-// document.body.appendChild(stats.dom);
-
-
 var animate = function () {
-    // stats.begin();
-
+    
     requestAnimationFrame(animate);
     controls.update();
     circles.map(x => x.lookAt(camera.position));
     renderer.render(scene, camera);
-
-    // stats.end();
+    
 };
 
 animate();
