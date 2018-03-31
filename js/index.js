1ba5c9ad788c1dba0f7523ca2a6b3b45a6911d98
Fred Hohman <fred.hohman@gmail.com>
2018-03-31 11:41:53 -0400

    clean up index.js

--- js/index.js
+++ js/index.js
@@ -3,11 +3,11 @@ import * as d3 from "d3";
 // global data and image paths
 
 // small
-// const dataDirName = 'lesmis' // good
-// const dataDirName = 'sec-sale' // good, labels
-// const dataDirName = 'sec-purchase' // good, labels
-// const dataDirName = 'moreno_names' // good
-// const dataDirName = 'ego-gplus' // good
+// const dataDirName = 'lesmis';
+// const dataDirName = 'sec-sale'; // labels
+// const dataDirName = 'sec-purchase'; // labels
+// const dataDirName = 'moreno_names';
+// const dataDirName = 'ego-gplus';
 
 // yelp
 // const dataDirName = 'yelp-k6d5'
@@ -30,17 +30,18 @@ import * as d3 from "d3";
 // const dataDirName = "yelp-k5d9";
 
 // medium
-// const dataDirName = "astroph"; // good
-// const dataDirName = 'glove' // good, labels
-// const dataDirName = 'com-amazon' // b
-// const dataDirName = 'stackexchange' // b
-// const dataDirName = 'word2vec-twitter' // kinda good, labels
+// const dataDirName = "astroph";
+const dataDirName = 'glove'; // labels
+// const dataDirName = 'com-amazon';
+// const dataDirName = 'stackexchange';
+// const dataDirName = 'word2vec-twitter'; // labels
 
 // large
-// const dataDirName = 'google-movie' // average, labels
-// const dataDirName = 'dblp' // b
-// const dataDirName = 'patentcite' // good
-const dataDirName = 'wikipedia-de' // good
+// const dataDirName = 'google-movie'; // labels
+// const dataDirName = 'dblp';
+// const dataDirName = 'patentcite';
+// const dataDirName = 'wikipedia-de';
+// const dataDirName = 'trackers-trackers';
 
 
 export const dataPath = "data/" + dataDirName + "/";
